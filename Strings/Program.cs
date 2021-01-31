a79f45ac0bfb159ec5d179fbbea749463cda87d1
Lemi02 <36259352+Lemi02@users.noreply.github.com>
2021-01-31 14:59:16 +0100

    Add project files.

---
+++ Strings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Trim, om spaties voor of achteraan een string te verwijderen
+            var testString = "  hjkhjkfhsd     ";
+            Console.WriteLine(testString);
+            Console.WriteLine(testString.Trim());
+            Console.WriteLine(testString.TrimEnd());
+            Console.WriteLine(testString.TrimStart());
+
+
+            var password = "P@ssw0rd";
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
